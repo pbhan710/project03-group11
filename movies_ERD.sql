4db15041708ba02f1b08ae098b42bd13ecd2e014
Paul Han <pbhan710@gmail.com>
2023-01-14 19:53:26 -0500

    Finalized build_database.ipynb file. Added raw JSON files.

---
+++ movies_ERD.sql
@@ -0,0 +1,46 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/5ktCGG
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Drop the 'movie' table if it already exists.
+DROP TABLE IF EXISTS movie CASCADE;
+
+CREATE TABLE "movie" (
+    "id" int NOT NULL,
+    "title" varchar NOT NULL,
+    "tagline" varchar NOT NULL,
+    "overview" varchar NOT NULL,
+    "homepage" varchar NOT NULL,
+    "release_date" date NOT NULL,
+    "budget" money NOT NULL,
+    "revenue" money NOT NULL,
+    "popularity" float NOT NULL,
+    "runtime" int NOT NULL,
+    "poster_path" varchar,
+    PRIMARY KEY (id)
+);
+
+-- Drop the 'actor' table if it already exists.
+DROP TABLE IF EXISTS actor;
+
+-- Actors and the movies in which they starred.
+CREATE TABLE "actor" (
+    "id" int NOT NULL,
+    "name" varchar NOT NULL,
+    "profile_path" varchar,
+    "movie_id" int NOT NULL,
+    "character" varchar NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
+);
+
+-- Drop the 'director' table if it already exists.
+DROP TABLE IF EXISTS director;
+
+-- Directors and movies that they directed.
+CREATE TABLE "director" (
+    "id" int NOT NULL,
+    "name" varchar NOT NULL,
+    "profile_path" varchar,
+    "movie_id" int NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
+);
